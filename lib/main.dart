30235960a148c2dfa645e19fd0e516f26a2b9ddc
zestones <idrissbenguezzou@gmail.com>
2024-05-23 02:59:46 +0200

    refactor: move injection logic to serviceInjection

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'dart:io';
 
+import 'package:namer_app/service/service_injection.dart';
 import 'package:namer_app/screens/contacts_screen.dart';
-import 'package:namer_app/service/contact_list_service.dart';
-import 'package:namer_app/service/contact_service.dart';
-import 'package:namer_app/service/group_service.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
 
@@ -12,59 +10,15 @@ import 'package:namer_app/service/load_csv_contact_list_service.dart';
 import 'package:namer_app/service/local_notification_service.dart';
 
 // Screens
-// import 'screens/contacts_screen.dart';
 import 'screens/settings/main_settings_screen.dart';
 import 'screens/home_screen.dart';
 
-import 'package:namer_app/repositories/contact_list_repository.dart';
-import 'package:namer_app/repositories/contact_repository.dart';
-import 'package:namer_app/repositories/group_repository.dart';
-
 // Utils
 import 'package:namer_app/utils/database_helper.dart';
+import 'package:namer_app/utils/themes.dart';
 
 // Widgets
 import 'widgets/navigation.dart';
-
-final ThemeData lightTheme = ThemeData(
-  useMaterial3: true,
-  colorScheme: ColorScheme.light(
-    primary: Color(0xFF2F80ED),
-    background: Color(0xFFFCFCFC),
-    onBackground: Color(0xFF4F5E7B),
-    secondary: Color(0xFFA7AFBE),
-  ),
-  primaryColor: Color(0xFF2F80ED),
-  scaffoldBackgroundColor: Color(0xFFFCFCFC),
-  iconTheme: IconThemeData(
-    color: Color(0xFF2F80ED),
-  ),
-  textTheme: TextTheme(
-    bodyLarge: TextStyle(color: Color(0xFF4F5E7B)),
-    bodyMedium: TextStyle(color: Color(0xFF4F5E7B)),
-  ),
-  fontFamily: 'Roboto',
-);
-
-final ThemeData darkTheme = ThemeData(
-  useMaterial3: true,
-  colorScheme: ColorScheme.dark(
-    primary: Color.fromRGBO(48, 150, 207, 1),
-    background: Color(0xFF121212),
-    onBackground: Colors.white,
-    secondary: Colors.grey,
-  ),
-  primaryColor: Color.fromRGBO(48, 150, 207, 1),
-  scaffoldBackgroundColor: Color(0xFF121212),
-  iconTheme: IconThemeData(
-    color: Color.fromRGBO(48, 150, 207, 1),
-  ),
-  textTheme: TextTheme(
-    bodyLarge: TextStyle(color: Colors.white),
-    bodyMedium: TextStyle(color: Colors.white),
-  ),
-  fontFamily: 'Roboto',
-);
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -75,44 +29,7 @@ void main() async {
 
   runApp(
     MultiProvider(
-      providers: [
-        Provider<ContactListRepository>(
-            create: (_) => ContactListRepository(databaseHelper)),
-        Provider<ContactRepository>(
-            create: (_) => ContactRepository(databaseHelper)),
-        Provider<GroupRepository>(
-            create: (_) => GroupRepository(databaseHelper)),
-
-        // Initialize the services, depending on repositories
-        ProxyProvider<ContactListRepository, ContactListService>(
-          update: (context, contactListRepository, _) =>
-              ContactListService(contactListRepository),
-        ),
-        ProxyProvider<ContactRepository, ContactService>(
-          update: (context, contactRepository, _) =>
-              ContactService(contactRepository),
-        ),
-        ProxyProvider<GroupRepository, GroupService>(
-          update: (context, groupRepository, _) =>
-              GroupService(groupRepository),
-        ),
-
-        ProxyProvider3<ContactListRepository, ContactRepository,
-            GroupRepository, LoadCSVContactListService>(
-          update: (
-            context,
-            contactListRepository,
-            contactRepository,
-            groupRepository,
-            _,
-          ) =>
-              LoadCSVContactListService(
-            contactListRepository,
-            contactRepository,
-            groupRepository,
-          ),
-        ),
-      ],
+      providers: ServiceInjection.inject(databaseHelper),
       child: MyApp(),
     ),
   );
---
+++ lib/service/service_injection.dart
@@ -0,0 +1,65 @@
+import 'package:namer_app/repositories/contact_list_repository.dart';
+import 'package:namer_app/repositories/contact_repository.dart';
+import 'package:namer_app/repositories/group_repository.dart';
+import 'package:namer_app/service/contact_list_service.dart';
+import 'package:namer_app/service/contact_service.dart';
+import 'package:namer_app/service/group_service.dart';
+import 'package:namer_app/service/load_csv_contact_list_service.dart';
+import 'package:namer_app/utils/database_helper.dart';
+
+import 'package:provider/single_child_widget.dart';
+import 'package:provider/provider.dart';
+
+class ServiceInjection {
+  static List<SingleChildWidget> inject(DatabaseHelper databaseHelper) {
+    return [
+      // initialize the repositories
+      ..._injectRepositories(databaseHelper),
+
+      // Initialize the services, depending on repositories
+      ..._injectServices(),
+    ];
+  }
+
+  static List<SingleChildWidget> _injectRepositories(
+      DatabaseHelper databaseHelper) {
+    return [
+      Provider<ContactListRepository>(
+          create: (_) => ContactListRepository(databaseHelper)),
+      Provider<ContactRepository>(
+          create: (_) => ContactRepository(databaseHelper)),
+      Provider<GroupRepository>(create: (_) => GroupRepository(databaseHelper)),
+    ];
+  }
+
+  static List<SingleChildWidget> _injectServices() {
+    return [
+      ProxyProvider<ContactListRepository, ContactListService>(
+        update: (context, contactListRepository, _) =>
+            ContactListService(contactListRepository),
+      ),
+      ProxyProvider<ContactRepository, ContactService>(
+        update: (context, contactRepository, _) =>
+            ContactService(contactRepository),
+      ),
+      ProxyProvider<GroupRepository, GroupService>(
+        update: (context, groupRepository, _) => GroupService(groupRepository),
+      ),
+      ProxyProvider3<ContactListRepository, ContactRepository, GroupRepository,
+          LoadCSVContactListService>(
+        update: (
+          context,
+          contactListRepository,
+          contactRepository,
+          groupRepository,
+          _,
+        ) =>
+            LoadCSVContactListService(
+          contactListRepository,
+          contactRepository,
+          groupRepository,
+        ),
+      ),
+    ];
+  }
+}
